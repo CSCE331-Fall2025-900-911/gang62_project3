3facbc3dffa2462e60723769af52b06116646944
David Zhang <theredhairedyonko@gmail.com>
2025-10-27 13:24:47 -0500

    feat(basic react framework added - no functionality) - ready for testing with backend

---
+++ project3_gang62/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  const employeeCount = 42;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          We have {employeeCount} employees working hard!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ project3_gang62/src/App.test.js
@@ -0,0 +1,10 @@
+// this project might not need extensive tests, but here's a basic test setup
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
