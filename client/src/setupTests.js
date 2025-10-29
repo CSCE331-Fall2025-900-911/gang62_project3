f2e513679ab31135bcea1f8673ce23e31f55892f
David Zhang <theredhairedyonko@gmail.com>
2025-10-29 13:23:23 -0500

    feat(generic react app)

---
+++ client/src/setupTests.js
@@ -0,0 +1,8 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+
+// --- IGNORE --- 
+// this file is for setting up tests and is not part of the main application code
+import '@testing-library/jest-dom';
