6b8ee69132cb4ac4bfe531a18d1c03f6ed2db841
Michael <clunky_plunging813@simplelogin.com>
2025-12-08 21:04:58 -0600

    fix: disable secure cookies

--- server/src/server.js
+++ server/src/server.js
@@ -85,7 +85,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            secure: process.env.NODE_ENV === 'production',
+            secure: false,
             sameSite: 'lax',
             httpOnly: true,
         },
